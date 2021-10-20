f9d86a2c621f558cec1b1a40a9d1e15d293b00e3
VV <Gmail>
2021-10-20 23:22:33 +0800

    20211020 主页siderbar

--- blog/src/views/home/index.vue
+++ blog/src/views/home/index.vue
@@ -5,7 +5,7 @@
                 <span>VRzheng博客</span>
             </div>
             <el-menu>
-
+ <sidebar-item v-for="item in routers" :key="item.path" :item='item' ></sidebar-item>
             </el-menu>
         </el-scrollbar>
 
@@ -23,7 +23,7 @@
                         <span></span>
                     </div> -->
                 </div>
-                <!-- <div class="header-tabs-box">
+                <div class="header-tabs-box">
                     <el-breadcrumb class="app-levelbar" separator="/">
                         <el-breadcrumb-item v-for="(item,index)  in levelList" :key="item.path">
                         <span v-if='item.redirect==="noredirect"||index==levelList.length-1'
@@ -31,23 +31,22 @@
                             <router-link v-else :to="item.redirect||item.path">{{item.name}}</router-link>
                         </el-breadcrumb-item>
                     </el-breadcrumb>
-                </div> -->
+                </div>
 
                 <div class="header-right">
-                    <!-- <el-dropdown trigger="click">
+                    <el-dropdown trigger="click">
                         <span>{{username}}<i class="el-icon-arrow-down el-icon--right"></i></span>
                         <el-dropdown-menu slot="dropdown">
                             <el-dropdown-item><span @click="handlePassword">修改密码</span></el-dropdown-item>
                             <el-dropdown-item><span @click="loginOut">退出登录</span></el-dropdown-item>
                             <el-dropdown-item disabled divided>主题切换</el-dropdown-item>
                         </el-dropdown-menu>
-                    </el-dropdown> -->
+                    </el-dropdown>
                 </div>
-
-
+                
                 <!--新增界面-->
-                <el-dialog title="修改密码" :visible.sync="passwordFormVisible" width="85%" top="5vh">
-                    <el-form :model="passwordFormData" :rules="passwordFormDataRules" ref="passwordFormData">
+                <el-dialog title="修改密码"  width="85%" top="5vh">
+                    <el-form >
                         <!-- <el-form-item label="原始密码" prop="old_password">
                             <el-input type="password" v-model="passwordFormData.old_password" auto-complete="off"></el-input>
                         </el-form-item>
@@ -63,13 +62,9 @@
                         <el-button type="primary" @click.native="addSubmit('passwordFormData')" :loading="passwordLoading">提交</el-button>
                     </div> -->
                 </el-dialog>
-
             </el-header>
-
             <!--遮板-->
-            <!-- <div class="main-mask"
-                 v-show="menuShow"
-                 @click="menuShow = !menuShow"></div> -->
+            <div class="main-mask"></div>
 
             <el-main class="main">
                 <transition name="move" mode="out-in">
@@ -81,9 +76,173 @@
 </template>
 
 <script>
-export default {};
+import SidebarItem from "./SidebarItem.vue";
+export default {
+  components: {
+    SidebarItem
+  }
+};
 </script>
 
 
 <style lang="scss">
+@import "../../style/mixin";
+.el-header {
+  /*background-color: #eef1f6;*/
+  color: #333;
+  padding: 0 10px;
+}
+
+.el-aside {
+  color: #333;
+}
+
+.el-main {
+  position: relative;
+  padding: 0 10px 10px;
+  background-color: #fff;
+  margin-left: 7px;
+  margin-top: 5px;
+}
+
+.main-mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 200%;
+  z-index: 100;
+  background-color: #000;
+  opacity: 0.3;
+}
+
+.move-enter-active,
+.move-leave-active {
+  transition: opacity 0.5s;
+}
+
+.move-enter,
+.move-leave {
+  opacity: 0;
+}
+
+.wrapper {
+  height: 100%;
+  overflow-y: hidden;
+  background-color: #f5f7f9;
+  font-color: #000;
+}
+
+.container-box {
+  height: 100%;
+  transition: -webkit-transform 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out, -webkit-transform 0.3s ease-in-out;
+  overflow-y: auto;
+}
+
+.container-left {
+  background-color: rgb(48, 65, 86);
+  transition: left 0.23s ease-in-out;
+  height: 100%;
+  flex: 0 0 auto;
+}
+.container-left-wrap {
+  overflow-x: hidden !important;
+}
+
+/*宽屏时出现*/
+.slide-toggle {
+  display: inline-block;
+  height: $header-height;
+  line-height: $header-height;
+  text-align: center;
+  cursor: pointer;
+}
+
+.slide-toggle span {
+  display: inline-block;
+  width: 2px;
+  height: 12px;
+  background-color: rgba(135, 141, 153, 0.8);
+  margin-left: 5px;
+}
+.slide-toggle-open span {
+  display: block;
+  margin-left: 4px;
+  margin-top: 4px;
+  width: 16px;
+  height: 2px;
+}
+.slide-toggle-open {
+  padding-top: 17px;
+}
+
+@media screen and (min-width: 768px) {
+  .main-mask {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .main {
+    overflow-y: scroll !important;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  .slide-hide.slide-in-left {
+    -webkit-transform: translate3d(64px, 0, 0) !important;
+    transform: translate3d(64px, 0, 0) !important;
+  }
+  .container-left.slide-hide.slide-in-left {
+    left: -64px;
+    width: 64px;
+  }
+  .container-left.slide-in-left {
+    left: 0;
+  }
+  .container-left {
+    margin-right: 0;
+    opacity: 1;
+    position: absolute !important;
+    top: 0;
+    left: -225px;
+    z-index: 10;
+    padding: 0;
+  }
+
+  .container-box.slide-in-left {
+    min-width: 0;
+    opacity: 1;
+    -webkit-transform: translate3d(187px, 0, 0);
+    transform: translate3d(187px, 0, 0);
+    overflow: initial;
+  }
+  .header-toggle {
+    display: block;
+    /*background-color: #26a2ff;*/
+    border-radius: 4px;
+    /*border: 1px solid #fff;*/
+    height: 40px;
+    margin: 10px 0;
+    padding: 2px 6px;
+    outline: none;
+    width: 40px;
+    z-index: 10;
+  }
+
+  .header-toggle span {
+    display: block;
+    width: 100%;
+    height: 4px;
+    margin: 5px auto;
+    background-color: rgba(135, 141, 153, 0.8);
+  }
+
+  .logo {
+    display: none;
+  }
+  .slide-toggle {
+    display: none;
+  }
+}
 </style>
